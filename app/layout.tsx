1acf26e98101c31bf40fdb31a92477521fe637c9
somnang <somnang.dev@gmail.com>
2025-02-24 15:53:20 +0700

    update some

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import AuthProvider from "@/component/AuthProvider";
+import Providers from "@/provider/Provider";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,9 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthProvider>
+        <Providers>
           <main>{children}</main>
-        </AuthProvider>
+        </Providers>
       </body>
     </html>
   );
--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,20 @@
-export { default } from "next-auth/middleware";
+import withAuth from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
+export default withAuth(
+  function middleware(req) {
+    // If the user is authenticated and tries to access /login, redirect to home
+    if (req.nextUrl.pathname.startsWith("/login") && req.nextauth.token) {
+      return NextResponse.redirect(new URL("/home", req.url));
+    }
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+);
 export const config = {
   matcher: ["/((?!login|api|public).*)"],
 };
