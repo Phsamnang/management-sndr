6b5fcc474796055fe6212f2d6935672a9fecff11
somnang <somnang.dev@gmail.com>
2025-08-01 11:02:57 +0700

    update

--- components/ui/pos/pos-interface.tsx
+++ components/ui/pos/pos-interface.tsx
@@ -56,7 +56,7 @@ export default function TableSelection() {
 
   useEffect(()=>{
     useClient.invalidateQueries({queryKey:['table']})
-  },[])
+  },[tableInfo])
 
   // Initial table data with dynamic categories
   const [tableData, setTableData] = useState<Table[]>([
